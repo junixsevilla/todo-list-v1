6144f1f56c996b5ea4f3564b5713a04ca4a3360c
Junix <jaranco@gmail.com>
2021-03-18 19:54:11 +1300

    adding all files

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+var items= ["Buy Food", "Cook Food", "Eat Food"];
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get ("/", function(req , res) {
+    //res.send("Server is running good!");
+
+    var today = new Date();
+ 
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    var day = today.toLocaleDateString("en-US", options);
+    
+
+    res.render("list", {kindOfDay: day, newListItems: items });    
+
+}); 
+
+
+app.post("/", function(req, res) {   
+    var item = req.body.newItem;
+    item = items.push(item);
+    res.redirect("/");
+    
+});
+
+app.listen(3000, function (){
+    console.log("Server running at port 3000");
+});

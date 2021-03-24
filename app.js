40fb568e86357d32cc351336553ee8a3e80b1f7c
Junix <jaranco@gmail.com>
2021-03-24 22:52:56 +1300

    complete files. add ejs templates/layout and module

--- app.js
+++ app.js
@@ -1,39 +1,54 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js")
+
 
 const app = express();
 
-var items= ["Buy Food", "Cook Food", "Eat Food"];
+// const is used. it is okay when pushing item into the array.
+const items= ["Buy Food", "Cook Food", "Eat Food"]; 
+const workItems = ["Code", "Test"];
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get ("/", function(req , res) {
-    //res.send("Server is running good!");
 
-    var today = new Date();
- 
-    var options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
+    const day = date.getDate(); // calling the date functions from date.js
 
-    var day = today.toLocaleDateString("en-US", options);
-    
-
-    res.render("list", {kindOfDay: day, newListItems: items });    
+    res.render("list", {listTitle: day, newListItems: items });    
 
 }); 
 
 
 app.post("/", function(req, res) {   
-    var item = req.body.newItem;
-    item = items.push(item);
+    const item = req.body.newItem;
+
+    if (req.body.list === "Work") { // console log first to check the correct value
+
+        workItems.push(item);
+        res.redirect("/work");
+        
+    } else {
+    items.push(item);
     res.redirect("/");
     
+    };
+
+    console.log(req.body);
+
 });
+
+app.get("/work", function(req, res) {
+    res.render("list", {listTitle: "Work List", newListItems: workItems } );
+});
+
+
+app.get("/about", function(req, res) {
+    res.render("about");
+})
 
 app.listen(3000, function (){
     console.log("Server running at port 3000");
